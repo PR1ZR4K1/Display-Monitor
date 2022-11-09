db67f11023d3eb3630f53564b6bdaa8e80cf5bf7
PR1Z4R4K1 <jaylonignacio@gmail.com>
2022-11-08 20:34:03 -0800

    Syntax Error Fix...

--- monitor.sh
+++ monitor.sh
@@ -6,20 +6,20 @@ primary=$(xrandr | grep -w connected | grep primary | awk '{print $1;}')
 secondary=$(xrandr | grep -w connected | grep HDMI | awk '{print $1;}')
 
 # 1 is the first parameter inputted when script is called
-if [ $1 -eq '' ]
-	#duplicate display
-	xrandr --output $secondary --auto --same-as $primary	
-elif [ $1 -eq "right" ]
-then
-	#new screen to the right of primary
-	xrandr --output $secondary --auto --right-of $primary
-elif [ $1 -eq "off" ]
-then
-	#turns secondary screen off
-	xrandr --output $secondary --off
-elif [ $1 -eq "left" ]
+if [ $1 == "left" ]
 then
 	#new screen to the left of primary
 	xrandr --output $secondary --auto --left-of $primary
-
+	
+elif [ $1 == "right" ]
+then
+	#new screen to the right of primary
+	xrandr --output $secondary --auto --right-of $primary
+elif [ $1 == "off" ]
+then
+	#turns secondary screen off
+	xrandr --output $secondary --off
+else
+	#duplicate display
+	xrandr --output $secondary --auto --same-as $primary
 fi
